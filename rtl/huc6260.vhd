122f6c64bb4e122f1a54920be81482cf483cd7a4
sorgelig <pour.garbage@gmail.com>
2020-05-20 00:13:59 +0800

    VCE: adjust vblank.

--- rtl/huc6260.vhd
+++ rtl/huc6260.vhd
@@ -78,7 +78,7 @@ constant HS_CLOCKS		: integer := 192;
 
 constant TOTAL_LINES		: integer := 263;  -- 525
 constant VS_LINES			: integer := 3; 	 -- pcetech.txt
-constant TOP_BL_LINES_E	: integer := 16;   -- pcetech.txt (must include VS_LINES in current implementation)
+constant TOP_BL_LINES_E	: integer := 18;   -- pcetech.txt (must include VS_LINES in current implementation)
 constant DISP_LINES_E	: integer := 242;	 -- same as in mednafen
 signal TOP_BL_LINES		: integer;
 signal DISP_LINES			: integer;
