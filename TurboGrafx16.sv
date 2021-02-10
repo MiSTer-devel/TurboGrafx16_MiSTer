7851c7f09ed01c183ced32df84b9c6347ea9da2b
sorgelig <pour.garbage@gmail.com>
2021-02-10 23:32:06 +0800

    Update sys. Crop.

--- TurboGrafx16.sv
+++ TurboGrafx16.sv
@@ -52,6 +52,36 @@ module emu
 	output [1:0]  VGA_SL,
 	output        VGA_SCALER, // Force VGA scaler
 
+	input  [11:0] HDMI_WIDTH,
+	input  [11:0] HDMI_HEIGHT,
+
+`ifdef USE_FB
+	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
+	// FB_FORMAT:
+	//    [2:0] : 011=8bpp(palette) 100=16bpp 101=24bpp 110=32bpp
+	//    [3]   : 0=16bits 565 1=16bits 1555
+	//    [4]   : 0=RGB  1=BGR (for 16/24/32 modes)
+	//
+	// FB_STRIDE either 0 (rounded to 256 bytes) or multiple of pixel size (in bytes)
+	output        FB_EN,
+	output  [4:0] FB_FORMAT,
+	output [11:0] FB_WIDTH,
+	output [11:0] FB_HEIGHT,
+	output [31:0] FB_BASE,
+	output [13:0] FB_STRIDE,
+	input         FB_VBL,
+	input         FB_LL,
+	output        FB_FORCE_BLANK,
+
+	// Palette control for 8bit modes.
+	// Ignored for other video modes.
+	output        FB_PAL_CLK,
+	output  [7:0] FB_PAL_ADDR,
+	output [23:0] FB_PAL_DOUT,
+	input  [23:0] FB_PAL_DIN,
+	output        FB_PAL_WR,
+`endif
+
 	output        LED_USER,  // 1 - ON, 0 - OFF.
 
 	// b[1]: 0 - LED status is system status OR'd with b[0]
@@ -81,6 +111,7 @@ module emu
 	output        SD_CS,
 	input         SD_CD,
 
+`ifdef USE_DDRAM
 	//High latency DDR3 RAM interface
 	//Use for non-critical time purposes
 	output        DDRAM_CLK,
@@ -93,7 +124,9 @@ module emu
 	output [63:0] DDRAM_DIN,
 	output  [7:0] DDRAM_BE,
 	output        DDRAM_WE,
+`endif
 
+`ifdef USE_SDRAM
 	//SDRAM interface with lower latency
 	output        SDRAM_CLK,
 	output        SDRAM_CKE,
@@ -106,6 +139,20 @@ module emu
 	output        SDRAM_nCAS,
 	output        SDRAM_nRAS,
 	output        SDRAM_nWE,
+`endif
+
+`ifdef DUAL_SDRAM
+	//Secondary SDRAM
+	input         SDRAM2_EN,
+	output        SDRAM2_CLK,
+	output [12:0] SDRAM2_A,
+	output  [1:0] SDRAM2_BA,
+	inout  [15:0] SDRAM2_DQ,
+	output        SDRAM2_nCS,
+	output        SDRAM2_nCAS,
+	output        SDRAM2_nRAS,
+	output        SDRAM2_nWE,
+`endif
 
 	input         UART_CTS,
 	output        UART_RTS,
@@ -122,9 +169,7 @@ module emu
 	input   [6:0] USER_IN,
 	output  [6:0] USER_OUT,
 
-	input         OSD_STATUS,
-
-	output reg  [7:0] stat_cnt, comm_cnt
+	input         OSD_STATUS
 );
 
 
@@ -148,16 +193,32 @@ assign LED_POWER = 0;
 assign BUTTONS   = osd_btn;
 assign VGA_SCALER= 0;
 
-wire [1:0] ar = status[25:24];
+wire [1:0] ar       = status[25:24];
+wire       vcrop_en = status[32];
+wire [3:0] vcopt    = status[36:33];
+reg        en216p;
+reg  [4:0] voff;
+always @(posedge CLK_VIDEO) begin
+	en216p <= ((HDMI_WIDTH == 1920) && (HDMI_HEIGHT == 1080) && !forced_scandoubler && !scale);
+	voff <= (vcopt < 6) ? {vcopt,1'b0} : ({vcopt,1'b0} - 5'd24);
+end
 
-assign VIDEO_ARX = (!ar) ? (overscan ? 8'd4 : 8'd47) : (ar - 1'd1);
-assign VIDEO_ARY = (!ar) ? (overscan ? 8'd3 : 8'd37) : 12'd0;
+wire vga_de;
+video_crop video_crop
+(
+	.*,
+	.VGA_DE_IN(vga_de),
+	.ARX((!ar) ? (overscan ? 8'd4 : 8'd47) : (ar - 1'd1)),
+	.ARY((!ar) ? (overscan ? 8'd3 : 8'd37) : 12'd0),
+	.CROP_SIZE((en216p & vcrop_en) ? 10'd216 : 10'd0),
+	.CROP_OFF(voff)
+);
 
 // Status Bit Map:
-// 0         1         2         3
-// 01234567890123456789012345678901
-// 0123456789ABCDEFGHIJKLMNOPQRSTUV
-// XXXXXXXXXXXXXXXXXXXXXX XXXXXX
+// 0         1         2         3          4         5         6
+// 01234567890123456789012345678901 23456789012345678901234567890123
+// 0123456789ABCDEFGHIJKLMNOPQRSTUV 0123456789ABCDEFGHIJKLMNOPQRSTUV
+// XXXXXXXXXXXXXXXXXXXXXX XXXXXX    XXXXX
 
 `include "build_id.v"
 parameter CONF_STR = {
@@ -184,6 +245,9 @@ parameter CONF_STR = {
 	"P1-;",
 	"P1OOP,Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
 	"P1O8A,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
+	"P1-;",
+	"d6P1o0,Vertical Crop,Disabled,216p(5x);",
+	"d6P1o14,Crop Offset,0,2,4,8,10,12,-12,-10,-8,-6,-4,-2;",
 	"P1-;",
 	"P1OS,Colors,Original,Raw RGB;",
 	"P1OH,Overscan,Hidden,Visible;",
@@ -258,7 +322,7 @@ pll pll
 
 ///////////////////////////////////////////////////
 
-wire [31:0] status;
+wire [63:0] status;
 wire  [1:0] buttons;
 
 wire [11:0] joy_0, joy_1, joy_2, joy_3, joy_4;
@@ -300,7 +364,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3), .WIDE(1)) hps_io
 
 	.buttons(buttons),
 	.status(status),
-	.status_menumask({snac, 1'd1, use_sdr, ~use_sdr, ~gg_avail,~bk_ena}),
+	.status_menumask({en216p, snac, 1'd1, use_sdr, ~use_sdr, ~gg_avail,~bk_ena}),
 	.forced_scandoubler(forced_scandoubler),
 
 	.sdram_sz(sdram_sz),
@@ -484,8 +548,6 @@ always @(posedge clk_sys) begin
 	cd_stat_rec <= 0;
 	cd_dataout_req <= 0;
 	if (reset || cart_download) begin
-		comm_cnt <= 0;
-		stat_cnt <= 0;
 		cd_region <= 0;
 	end
 	else begin
@@ -496,8 +558,6 @@ always @(posedge clk_sys) begin
 			cd_stat_rec <= ~cd_out[16];
 			cd_dataout_req <= cd_out[16];
 			cd_region <= cd_out[17];
-
-			stat_cnt <= stat_cnt + 8'd1;
 		end
 
 		cd_comm_send_old <= cd_comm_send;
@@ -508,15 +568,11 @@ always @(posedge clk_sys) begin
 			cd_in[95:0] <= cd_comm;
 			cd_in[111:96] <= {status[1],15'd0};
 			cd_in[112] <= ~cd_in[112];
-
-			comm_cnt <= comm_cnt + 8'd1;
 		end
 		else if (cd_dataout_send && !cd_dataout_send_old) begin
 			cd_in[79:0] <= cd_dataout;
 			cd_in[111:96] <= 16'h0001;
 			cd_in[112] <= ~cd_in[112];
-
-//			comm_cnt <= comm_cnt + 8'd1;
 		end
 		else if (cd_dat_req && !cd_dat_req_old) begin
 			cd_in[111:96] <= 16'h0002;
@@ -684,6 +740,7 @@ video_mixer #(.LINE_LENGTH(560), .GAMMA(1)) video_mixer
 	.clk_vid(CLK_VIDEO),
 	.ce_pix(ce_pix),
 	.ce_pix_out(CE_PIXEL),
+	.VGA_DE(vga_de),
 
 	.scanlines(0),
 	.scandoubler(scale || forced_scandoubler),
